a29c8d7b2c9250a1c2280886a88e4b835886c7ae
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-26 00:11:50 +0300

    Add task 54

---
+++ Task054/Program.cs
@@ -0,0 +1,44 @@
+﻿int m = 4, n = 4;                                                                                                                                   
+int[,] mass = new int[n, m];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{                                                                             
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        for (int k = 0; k < mass.GetLength(1); k++)
+        {
+            if (mass[i, j] <= mass[i, k]) continue;
+            int temp = mass[i, j];
+            mass[i, j] = mass[i, k];
+            mass[i, k] = temp;
+        }
+    }
+}
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
